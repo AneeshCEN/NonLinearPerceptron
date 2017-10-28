3017c64fbc8d468c80214403cfea60115dc01074
a4aneeshc <a4aneeshc@gmail.com>
2017-10-28 16:10:25 +0530

    code change

--- NonLinearPerceptron.py
+++ NonLinearPerceptron.py
@@ -7,12 +7,14 @@ Created on Wed Oct 25 12:02:49 2017
 """
 import numpy as np
 
-from sklearn.datasets import make_circles, make_blobs
+from sklearn.datasets import make_circles, make_blobs, make_moons
 import matplotlib.pyplot as plt
 from sklearn.linear_model import Perceptron
 np.random.seed(0)
 
-X,y = make_circles(n_samples=800,factor=0.3, noise=0.05)
+#X,y = make_circles(n_samples=800,factor=0.3, noise=0.05)
+
+X,y = make_moons(n_samples=300, shuffle=True, noise=0.3, random_state=None)
 
 #X, y = make_blobs(n_samples=1000,n_features=2,centers=2,cluster_std=1.0,random_state=20)
 
@@ -20,9 +22,7 @@ def feature_mapping(x1, x2, degree):
     phi_x = np.array([])
     for i in range(1, degree+1):
         for j in range(i+1):
-            print ('x1^'+str(i-j)+',x2^'+str(j))
             mapped_feature=(x1**(i-j))*(x2**(j))
-            print x1
             if len(phi_x) == 0:
                 phi_x = mapped_feature
             else:
@@ -35,7 +35,7 @@ def feature_mapping(x1, x2, degree):
 plt.scatter(X[y==1,0], X[y==1,1], c='r', label='class 1')
 plt.scatter(X[y==0,0], X[y==0,1], c='g', label='class 2')
 
-phi_x = feature_mapping(X[:,0], X[:,1], 2)
+phi_x = feature_mapping(X[:,0], X[:,1], 10)
 
 plt.xlabel('X - axis')
 plt.ylabel('Y - axis')
@@ -50,7 +50,9 @@ x1_min, x1_max = X[:, 0].min(), X[:, 0].max()
 x2_min, x2_max = X[:, 1].min(), X[:, 1].max()
 xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
 np.arange(x2_min, x2_max, resolution))
-Z = clf.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+K = np.array([xx1.ravel(), xx2.ravel()]).T
+test_feat = feature_mapping(K[:,0],K[:,1],10)
+Z = clf.predict(test_feat)
 Z = Z.reshape(xx1.shape)
 plt.contourf(xx1, xx2, Z, alpha=0.4)
 plt.xlim(xx1.min(), xx1.max())
